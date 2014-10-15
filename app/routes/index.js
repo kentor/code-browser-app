1eb145a59e9d4ab86c8f87e54e81a4e1644825b6
Kenneth Chung <ken70r@gmail.com>
2014-10-14 18:49:09 -0700

    supply `branchAndPath` to index router when transitioning to tree

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    this.transitionTo('tree', Ember.Object.create({ branch: 'master' }));
+    this.transitionTo('tree', 'master');
   }
 });
